93c9a01f2b85d1b9521fb9a62615fc75f1ff21b7
Hugo DEMENEZ <hdemenez@hotmail.fr>
2023-02-20 19:49:03 +0100

    replacement

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-const carouselContainer =  document.getElementById("carouselContainer");
-
-
-console.log(carouselContainer.scrollLeft)
-console.log(carouselContainer.scrollWidth - carouselContainer.clientWidth)
-
-window.addEventListener('load', () => {
-    self.setInterval(() => {
-    if (carouselContainer.scrollLeft < carouselContainer.scrollWidth - carouselContainer.clientWidth) {
-        carouselContainer.scrollTo(carouselContainer.scrollLeft + 1, 0);
-    }
-    else{
-        carouselContainer.scrollTo(0, 0);
-    }
-    }, 20);
-}
-);
-
-
-
 const content = document.getElementById("content");
 
 document.addEventListener('scroll', (event) => {
@@ -27,10 +7,10 @@ document.addEventListener('scroll', (event) => {
     )
     
     let index = Math.round((content.children.length-1)*scrolled,0)
-    if(index-1>=0){
+    if(index-1>=0 && content.children[index-1].className!=="cta"){
         content.children[index-1].style.color = "lightgray"
     }
-    if(index+1<=content.children.length-1){
+    if(index+1<=content.children.length-1 && content.children[index-1].className!=="cta"){
         content.children[index+1].style.color = "lightgray"
     }
     content.children[index].style.color = "black"

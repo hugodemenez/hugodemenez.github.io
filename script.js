4802af469fafb0bc7f50b999dd442614375c7968
Hugo DEMENEZ <hdemenez@hotmail.fr>
2023-02-21 17:45:49 +0100

    test

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const content = document.getElementById("content");
+// Auto Scrolling Caroussel
+const carouselContainer = document.getElementById("carouselContainer")
 
+window.addEventListener('load', () => {
+    self.setInterval(() => {
+    if (carouselContainer.scrollLeft !== carouselContainer.scrollWidth - carouselContainer.clientWidth) {
+        carouselContainer.scrollTo(carouselContainer.scrollLeft + carouselContainer.clientWidth, 0);
+    }
+    }, 6000);
+});
+
+
+// Font Highlight onScroll
+const content = document.getElementById("content");
 document.addEventListener('scroll', (event) => {
     let scrolled = 
     document.documentElement.scrollTop / (
@@ -28,6 +40,8 @@ document.addEventListener('scroll', (event) => {
 })
 
 
+
+// Number field animation
 function showNumberField(){
     console.log("Show number")
     const input = document.getElementById("number")
@@ -36,3 +50,14 @@ function showNumberField(){
         console.log("animate !!")
     }
 }
+
+//Image hover effect
+const project1 = document.getElementById("project1")
+project1.addEventListener("mouseenter",(event) => {
+    console.log("GOT IT")
+    event.target.src = "./hover_image.png"
+    // reset the color after a short delay
+    setTimeout(() => {
+        event.target.src = "./image.png"
+    }, 500);
+}, false);

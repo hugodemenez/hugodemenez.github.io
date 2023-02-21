f38bcd328bb1fc7c965a2c954c3476750ab90454
Hugo DEMENEZ <hdemenez@hotmail.fr>
2023-02-21 17:15:01 +0100

    new input animation

--- script.js
+++ script.js
@@ -7,13 +7,32 @@ document.addEventListener('scroll', (event) => {
     )
     
     let index = Math.round((content.children.length-1)*scrolled,0)
-    if(index-1>=0 && content.children[index-1].className!=="cta"){
+    if(index-1>=0 
+        && content.children[index-1].className!=="cta"
+        && content.children[index-1].className!=="title"
+        ){
         content.children[index-1].style.color = "lightgray"
     }
-    if(index+1<=content.children.length-1 && content.children[index+1].className!=="cta"){
+    if(index+1<=content.children.length-1 
+        && content.children[index+1].className!=="cta"
+        && content.children[index+1].className!=="title"
+        ){
         content.children[index+1].style.color = "lightgray"
     }
-    if(content.children[index].className!=="cta"){
+    if(
+        content.children[index].className!=="cta" 
+        && content.children[index].className!=="title"
+        ){
         content.children[index].style.color = "black"
     }
 })
+
+
+function showNumberField(){
+    console.log("Show number")
+    const input = document.getElementById("number")
+    if (!input.className.includes("open_animation")){
+        input.className = input.className + " open_animation";
+        console.log("animate !!")
+    }
+}

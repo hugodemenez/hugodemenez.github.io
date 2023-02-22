0c28bc441c337f44df6e0f2fb9a55112737a0c4b
Hugo DEMENEZ <hdemenez@hotmail.fr>
2023-02-22 11:40:44 +0100

    enhanced reading highlight and scroll effects

--- script.js
+++ script.js
@@ -4,57 +4,51 @@ const carouselContainer = document.getElementById("carouselContainer")
 window.addEventListener('load', () => {
     self.setInterval(() => {
     if (carouselContainer.scrollLeft !== carouselContainer.scrollWidth - carouselContainer.clientWidth) {
+        // Scroll to next picture
         carouselContainer.scrollTo(carouselContainer.scrollLeft + carouselContainer.clientWidth, 0);
     }
-    }, 6000);
+    else{
+        // Restart scroll to start
+        carouselContainer.scrollTo(0, 0);
+    }
+    }, 3000);
 });
 
 
 // Font Highlight onScroll
-const content = document.getElementById("content");
+const readingHighlight = document.getElementById("readingHighlight");
+
+
 document.addEventListener('scroll', (event) => {
     let scrolled = 
     document.documentElement.scrollTop / (
         document.documentElement.scrollHeight - document.documentElement.clientHeight
     )
-    
-    let index = Math.round((content.children.length-1)*scrolled,0)
-    if(index-1>=0 
-        && content.children[index-1].className!=="cta"
-        && content.children[index-1].className!=="title"
-        ){
-        content.children[index-1].style.color = "lightgray"
+    let index = Math.round((readingHighlight.children.length-1)*scrolled,0)
+    for (const child of readingHighlight.children){
+        if (
+            child.className!=="cta"
+            && child.className!=="title"
+        )
+        child.style.color = "lightgray";
     }
-    if(index+1<=content.children.length-1 
-        && content.children[index+1].className!=="cta"
-        && content.children[index+1].className!=="title"
-        ){
-        content.children[index+1].style.color = "lightgray"
-    }
-    if(
-        content.children[index].className!=="cta" 
-        && content.children[index].className!=="title"
-        ){
-        content.children[index].style.color = "black"
-    }
+    readingHighlight.children[index].style.color = "black"
+
 })
 
 
 
 // Number field animation
 function showNumberField(){
-    console.log("Show number")
     const input = document.getElementById("number")
     if (!input.className.includes("open_animation")){
         input.className = input.className + " open_animation";
-        console.log("animate !!")
     }
 }
 
 //Image hover effect
 const project1 = document.getElementById("project1")
 project1.addEventListener("mouseenter",(event) => {
-    console.log("GOT IT")
     event.target.src = "./hover_image.png"
     // reset the color after a short delay
     setTimeout(() => {

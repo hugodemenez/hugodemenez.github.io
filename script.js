bf705f031b3e22bc75a9602fe0c54a103a2ba542
Hugo DEMENEZ <hdemenez@hotmail.fr>
2023-04-30 10:58:58 +0200

    resolved message bar sticky

--- script.js
+++ script.js
@@ -33,48 +33,6 @@ window.addEventListener('load',
 );
 
 
-// Font Highlight onScroll
-const readingHighlight = document.getElementById("readingHighlight");
-
-
-document.addEventListener('scroll', (event) => {
-    let scrolled = 
-    document.documentElement.scrollTop / (
-        document.documentElement.scrollHeight - document.documentElement.clientHeight
-    )
-    let index = Math.round((readingHighlight.children.length-1)*scrolled,0)
-    for (const child of readingHighlight.children){
-        if (
-            child.className!=="cta"
-            && child.className!=="title"
-        )
-        child.style.color = "rgb(var(--text))";
-    }
-    readingHighlight.children[index].style.color = "rgb(var(--accent))"
-
-})
-
-
-
-// Number field animation
-function showNumberField(){
-    const input = document.getElementById("number")
-    if (!input.className.includes("open_animation")){
-        input.className = input.className + " open_animation";
-    }
-}
-
-//Image hover effect
-const project1 = document.getElementById("project1")
-project1.addEventListener("mouseenter",(event) => {
-    event.target.src = "./hover_image.png"
-    // reset the color after a short delay
-    setTimeout(() => {
-        event.target.src = "./image.png"
-    }, 500);
-}, false);
-
-
 // Chat
 
 // Definning elements
